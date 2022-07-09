f38167f20b3168e3165079c6ed944263ef2fe3f9
ADITYA SINGH GANGWAR <103820233+gangwarady@users.noreply.github.com>
2022-07-09 10:46:50 +0530

    Create Little Elephant and Function.cpp

---
+++ Little Elephant and Function.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+# define ll long long
+using namespace std;
+
+int main()
+{
+  int n;
+  cin>>n;
+  cout<<n<<" ";
+  for(int i = 1; i<=n-1; i++){
+    cout<<i<<" ";
+  }
+}
